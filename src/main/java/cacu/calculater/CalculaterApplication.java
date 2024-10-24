db9eedaf77738540dfe4259523b5e068a69da8c1
jogarao10 <jogaraokorrayi107@gmail.com>
2024-10-24 21:23:12 +0530

    first commit

---
+++ src/main/java/cacu/calculater/CalculaterApplication.java
@@ -0,0 +1,13 @@
+package cacu.calculater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaterApplication.class, args);
+	}
+
+}
